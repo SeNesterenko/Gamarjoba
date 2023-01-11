5c549357cbe100e62788dd0726eb0b20ac3f0280
snesterenko <snesterenko@dtln.ru>
2023-01-11 18:27:23 +0300

    Jump implementation

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    [SerializeField] private float _jumpHeight;
+    [SerializeField] private float _gravity;
+    [SerializeField] private float _stepDown;
+    
+    private Vector3 velocity;
+    private bool _isJumping;
+    
+    private Vector3 _rootMotion;
+    private CharacterController _characterController;
+    
     private Animator _animator;
     private Vector2 _input;
     
     private static readonly int InputX = Animator.StringToHash("InputX");
     private static readonly int InputY = Animator.StringToHash("InputY");
+    private static readonly int IsJumping = Animator.StringToHash("isJumping");
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -20,5 +32,55 @@ public class PlayerMovementController : MonoBehaviour
         
         _animator.SetFloat(InputX, _input.x);
         _animator.SetFloat(InputY, _input.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isJumping)
+        {
+            velocity.y -= _gravity * Time.fixedDeltaTime;
+            _characterController.Move(velocity * Time.fixedDeltaTime);
+
+            _isJumping = !_characterController.isGrounded;
+            _rootMotion = Vector3.zero;
+            return;
+        }
+        
+        _characterController.Move(_rootMotion + Vector3.down * _stepDown);
+        _rootMotion = Vector3.zero;
+
+        if (!_characterController.isGrounded)
+        {
+            _isJumping = true;
+            _animator.SetBool(IsJumping, true);
+            velocity = _animator.velocity;
+            velocity.y = 0;
+        }
+        else
+        {
+            _animator.SetBool(IsJumping, false);
+        }
+    }
+
+    private void OnAnimatorMove()
+    {
+        _rootMotion += _animator.deltaPosition;
+    }
+
+    private void Jump()
+    {
+        if (!_isJumping)
+        {
+            _isJumping = true;
+            _animator.SetBool(IsJumping, true);
+            
+            velocity = _animator.velocity;
+            velocity.y = Mathf.Sqrt(2 * _gravity * _jumpHeight);
+        }
     }
 }
